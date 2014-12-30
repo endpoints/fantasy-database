e5e6580af247e9b9e3d5b184475af630700b6ab6
Tyler Kellen <tyler@sleekcode.net>
2014-12-30 16:20:35 -0500

    make schema sqlite3 friendly
    sqlite3 wont create an auto incrementing field using
    postgresql conventions (serial PRIMARY KEY)

--- schema.sql
+++ schema.sql
@@ -1,26 +1,26 @@
 CREATE TABLE authors (
-  id serial PRIMARY KEY,
+  id integer PRIMARY KEY,
   name text NOT NULL CHECK(name <> ''),
   date_of_birth date NOT NULL,
   date_of_death date
 );
 
 CREATE TABLE series (
-  id serial PRIMARY KEY,
+  id integer PRIMARY KEY,
   title text NOT NULL CHECK(title <> '')
 );
 
 CREATE TABLE books (
-  id serial PRIMARY KEY,
-  author_id int NOT NULL REFERENCES authors(id),
-  series_id int REFERENCES series(id),
+  id integer PRIMARY KEY,
+  author_id integer NOT NULL REFERENCES authors(id),
+  series_id integer REFERENCES series(id),
   date_published date NOT NULL,
   title text NOT NULL CHECK(title <> '')
 );
 
 CREATE TABLE chapters (
-  id serial PRIMARY KEY,
-  book_id int NOT NULL REFERENCES books(id),
+  id integer PRIMARY KEY,
+  book_id integer NOT NULL REFERENCES books(id),
   title text NOT NULL CHECK(title <> ''),
-  ordering int NOT NULL
+  ordering integer NOT NULL
 );
