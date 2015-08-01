a562f391fa27ddf312e50b7e8ee622b6ed8b8cb6
Ryan P Kilby <rpkilby@ncsu.edu>
2015-08-01 17:38:12 -0400

    Add Django fixture generation on setup

---
+++ django/utils.py
@@ -0,0 +1,74 @@
+
+
+def translate_fixture(data):
+    """
+    Translate the contents of the `data.json` file into a django compatible fixture.
+    """
+    results = []
+
+    for author in data['authors']:
+        results.append({
+            'pk': author['id'], 'model': 'fantasy.author',
+            'fields': {
+                'name': author['name'],
+                'date_of_birth': author['date_of_birth'],
+                'date_of_death': author['date_of_death'],
+            }
+        })
+
+    for series in data['series']:
+        results.append({
+            'pk': series['id'], 'model': 'fantasy.series',
+            'fields': {
+                'title': series['title'],
+            }
+        })
+
+    for book in data['books']:
+        results.append({
+            'pk': book['id'], 'model': 'fantasy.book',
+            'fields': {
+                'series': book['series_id'],
+                'author': book['author_id'],
+                'title': book['title'],
+                'date_published': book['date_published'],
+            }
+        })
+
+    for chapter in data['chapters']:
+        results.append({
+            'pk': chapter['id'], 'model': 'fantasy.chapter',
+            'fields': {
+                'title': chapter['title'],
+                'book': chapter['book_id'],
+                'ordering': chapter['ordering'],
+            }
+        })
+
+    for store in data['stores']:
+        results.append({
+            'pk': store['id'], 'model': 'fantasy.store',
+            'fields': {
+                'name': store['name'],
+                'books': [sb['book_id'] for sb in data['books_stores'] if sb['store_id'] == store['id']],
+            }
+        })
+
+    for photo in data['photos']:
+        types_map = {
+            'authors': ('fantasy', 'author'),
+            'books': ('fantasy', 'book'),
+            'series': ('fantasy', 'series'),
+        }
+
+        results.append({
+            'pk': photo['id'], 'model': 'fantasy.photo',
+            'fields': {
+                'imageable_id': photo['imageable_id'],
+                'imageable_type': types_map[photo['imageable_type']],
+                'title': photo['title'],
+                'uri': photo['uri'],
+            }
+        })
+
+    return results
--- setup.py
+++ setup.py
@@ -1,11 +1,46 @@
 import os
+import sys
+import json
 from setuptools import setup, find_packages
+from setuptools.command.develop import develop
+from setuptools.command.install import install
 
 
 README = open(os.path.join(os.path.dirname(__file__), 'django/README.md')).read()
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+# append the django directory to our path so we can import utils
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(os.path.join(BASE_DIR, 'django'))
+
+
+def build_fixture():
+    from utils import translate_fixture
+
+    contents = None
+    with open(os.path.join(BASE_DIR, 'data.json')) as f:
+        contents = json.loads(f.read())
+
+    contents = translate_fixture(contents)
+
+    with open(os.path.join(BASE_DIR, 'django/fantasy/fixtures/fantasy-database.json'), 'w') as f:
+        f.write(json.dumps(contents))
+
+
+class Develop(develop):
+
+    def run(self):
+        develop.run(self)
+        build_fixture()
+
+
+class Install(install):
+
+    def run(self):
+        install.run(self)
+        build_fixture()
 
 
 setup(
@@ -19,6 +54,11 @@ setup(
     author='Tyler Kellen',
     package_dir={'': 'django'},
     packages=find_packages(),
+
+    cmdclass={
+        'develop': Develop,
+        'install': Install,
+    },
 
     classifiers=[
         'Development Status :: 3 - Alpha',
